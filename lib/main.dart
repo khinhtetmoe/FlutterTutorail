44711924e6cda8cba370f739d72b98c43a813082
khinhtetmoe <khinkhinhtet65@gmail.com>
2021-06-19 18:26:38 +0900

    lesson8 commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,16 +14,11 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body:Center(
-        child: Text(
-          'hello ninjas',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower',
-          ),
-        ),
+        // child:Image(
+        //   // image:NetworkImage('https://images.unsplash.com/photo-1505506874110-6a7a69069a08?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80')
+        //      image:AssetImage('assets/space2.jpg'),
+        // ),
+        child:Image.asset('assets/space2.jpg'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
